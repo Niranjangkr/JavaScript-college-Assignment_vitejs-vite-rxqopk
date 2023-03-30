443cdd0c0f74e5ab8c2a1a648188eb389b3e4085
Niranjan Gaonkar <110449470+Niranjangkr@users.noreply.github.com>
2023-03-30 10:08:07 +0530

    Update FormSubmission.jsx

--- src/Components/FormSubmission.jsx
+++ src/Components/FormSubmission.jsx
@@ -1,29 +1,41 @@
-import React from "react"
+import React from 'react';
 
-class FormSubmission extends React.Component{
-  constructor(){
-    super()
-    this.state = {rollno: "", name: "", age: "", submitFlag: false} 
+class FormSubmission extends React.Component {
+  constructor() {
+    super();
+    this.state = { rollno: '', name: '', age: '', submitFlag: false };
   }
 
   handleChange = (e) => {
-      this.setState({[e.target.name]: e.target.value}) 
-  }
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state);
+  };
 
-  handleSubmit =(e) => {
-    e.prevetDefault()
-    this.setState({submitFlag: true})
-    const{rollno, name, age, submitFlag} = this.state;
-  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ submitFlag: true });
+    // const { rollno, name, age, submitFlag } = this.state;
+  };
 
-  render(){
+  render() {
     let details;
-    const {rollno, name, age, submitFlag}
-    if(submitFlag){
-      details = <h1>{name}-{rollno}</h1>
+    const { rollno, name, age, submitFlag } = this.state;
+    if (submitFlag) {
+      details = (
+        <h1>
+          {name}-{rollno}
+        </h1>
+      );
     }
-    return(
-      
-    )
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="rollno" onChange={this.handleChange} />
+        <input type="text" name="name" onChange={this.handleChange} />
+        <input type="text" name="age" onChange={this.handleChange} />
+        <button type="suubmit">Submit</button>
+        {details}
+      </form>
+    );
   }
 }
+export default FormSubmission;
